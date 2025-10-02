2279726ff5f8561a192e9eea1ad68dd16cfcb809
Mohammad Faraji <m.faraji9841@gmail.com>
2025-10-02 21:55:10 +0330

    Added TcpListener

--- OurFramework.NET/Framwork/Network/OurHttpListener.cs
+++ OurFramework.NET/Framwork/Network/OurHttpListener.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,15 @@ namespace OurFramework.NET.Framwork.Network
 {
     public class OurHttpListener
     {
+        private readonly List<string> _prefixes = [];
+        private readonly TcpListener _tcp;
+
+        public void  AddPrefix(string prefix)
+        {
+            if(string.IsNullOrEmpty(prefix))
+                throw new ArgumentException("prefix cannot  be null or empty ",nameof(prefix));
+
+            _prefixes.Add(prefix);
+        }
     }
 }
