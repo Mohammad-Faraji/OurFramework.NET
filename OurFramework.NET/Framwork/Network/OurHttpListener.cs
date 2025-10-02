6075cc578da2ad47ee6643bb83bf3d847c192a04
Mohammad Faraji <m.faraji9841@gmail.com>
2025-10-02 22:13:15 +0330

    Writing Method Start

--- OurFramework.NET/Framwork/Network/OurHttpListener.cs
+++ OurFramework.NET/Framwork/Network/OurHttpListener.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,31 @@ namespace OurFramework.NET.Framwork.Network
     public class OurHttpListener
     {
         private readonly List<string> _prefixes = [];
-        private readonly TcpListener _tcp;
+        private  TcpListener? _tcp;
 
-        public void  AddPrefix(string prefix)
+        public void AddPrefix(string prefix)
         {
-            if(string.IsNullOrEmpty(prefix))
-                throw new ArgumentException("prefix cannot  be null or empty ",nameof(prefix));
+            if (string.IsNullOrEmpty(prefix))
+                throw new ArgumentException("prefix cannot  be null or empty ", nameof(prefix));
 
             _prefixes.Add(prefix);
+        }
+
+        public void Start()
+        {
+            if(_tcp != null) throw new InvalidOperationException("Alrady Started!");
+            if (_prefixes.Count == 0) throw new InvalidOperationException("No Prefix Added");
+
+            var first = _prefixes[0];
+            var uri = new Uri(first);
+
+            var ipAddress = uri.Host.Equals("localhost", StringComparison.OrdinalIgnoreCase) switch
+            {
+                true => IPAddress.Loopback,
+               _ => IPAddress.Parse(first),
+            };
+
+            _tcp = new TcpListener(ipAddress, uri.Port);
         }
     }
 }
